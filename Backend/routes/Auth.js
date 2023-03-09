0e521c8cebe8eccf9a33fdde2c63a00e6d7b0ee7
Gaurav Lohar <114788180+lohargaurav00@users.noreply.github.com>
2023-03-09 22:18:48 +0530

    bcrypt user pass data sys done

--- Backend/routes/Auth.js
+++ Backend/routes/Auth.js
@@ -1,36 +1,44 @@
-const express = require('express')
-const Users = require('../models/Users')
-const router = express.Router()
-const { body, validationResult } = require('express-validator');
+const express = require("express");
+const Users = require("../models/Users");
+const router = express.Router();
+const { body, validationResult } = require("express-validator");
+const bcrypt = require('bcrypt');
 
-router.get('/',[
-   body('email',"Enter a valid email address").isEmail(),
-   body('password',"Enter a valid Password").isLength({ min: 8 }),
+router.get(
+  "/",
+  [
+    body("email", "Enter a valid email address").isEmail(),
+    body("password", "Enter a valid Password").isLength({ min: 8 }),
+  ],
 
-], 
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+    try {
+      let user = await Users.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .send({ error: "Email id already exit's!! use a valid email id" });
+      }
+
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password,salt);
+
+      user = await Users.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: secPass,
+      });
+      res.json(user);
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Some error occurred");
+    }
   }
+);
 
-  let user = await Users.findOne({email: req.body.email});
-  if(user){
-    return res.status(400).send({error: "Email id already exit's!! use a valid email id"});
-  }
-
-  user = await Users.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-  })
-  res.json(req.json(Users))
-  
-  // .then(Users=> res.json(Users))
-  // .catch(err=>{
-  // res.json({error: "Email id already exit's!! use a valid email id", message:err.message})
-  // });
-  })
-
-module.exports = router
+module.exports = router;
