1a1580ffbcbf75d55d4f0c7f9e19cff95037fba7
Gaurav Lohar <114788180+lohargaurav00@users.noreply.github.com>
2023-05-23 18:13:55 +0530

    bug fixes

--- Backend/db.js
+++ Backend/db.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose");
-const mongoUrl = 'mongodb://127.0.0.1:27017/Notes';
+const mongoUrl = "mongodb://127.0.0.1:27017/Notes";
 
-
-const connectTOMongo =async ()=>{
-     await mongoose.connect(mongoUrl);
-     console.log("connected to mongod");
-
-}
-
+const connectTOMongo = async () => {
+  try {
+    await mongoose.connect(mongoUrl);
+    console.log("connected to mongod");
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 module.exports = connectTOMongo;
